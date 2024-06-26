177bae6a6ebdb97aaea8a1cedd6e2547ec1ef87e
Samantha Diaz <20212025@itla.edu.do>
2024-06-25 21:53:57 -0400

    Agregando y removiendo clase. Tambien se agrego el objeto Audio

--- script.js
+++ script.js
@@ -4,22 +4,62 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
 
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+const inputEnfoqueMusica = document.querySelector('#alternar-musica');
+const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+
+musica.loop = true;
+
+inputEnfoqueMusica.addEventListener('change', () => {
+  if(musica.paused){
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
 botonCorto.addEventListener('click', () =>{
   cambiarContexto('descanso-corto');
+  botonCorto.classList.add('active');
 });
 
 
 botonEnfoque.addEventListener('click', () => {
-  cambiarContexto('enfoque');  
-})
+  cambiarContexto('enfoque');
+  botonEnfoque.classList.add('active');  
+});
 
 botonLargo.addEventListener('click', () => {
   cambiarContexto('descanso-largo');
+  botonLargo.classList.add('active');
 });
 
 
 function cambiarContexto (contexto){
+  
+  botones.forEach( function (contexto) {
+    contexto.classList.remove('active');
+  });
+
   html.setAttribute('data-contexto', contexto);
   banner.setAttribute('src', `./imagenes/${contexto}.png`);
+
+  switch (contexto) {
+    case "enfoque":
+      titulo.innerHTML =  `Optimiza tu productividad,<br>
+        <strong class="app__title-strong">sumérgete en lo que importa.</strong>`;
+      break;
+    case "descanso-corto":
+      titulo.innerHTML =  `¿Qué tal tomar un respiro? <br>
+        <strong class="app__title-strong">¡Haz una pausa corta!</strong>`;
+      break;  
+    case "descanso-largo":
+      titulo.innerHTML =  `Hora de volver a la superficie <br>
+        <strong class="app__title-strong"> Haz una pausa larga.</strong>`;
+      break;  
+  
+    default:
+      break;
+  }
 };
